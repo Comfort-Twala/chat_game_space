8f7cde142877cfed4e80f026bdd3eff117722092
Comfort-Twala <kontreitroos@gmail.com>
2021-12-15 12:18:35 +0200

    Server class created with basic methods

---
+++ Chat Game Space/src/Server.java
@@ -0,0 +1,46 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ * Server class to listen for new clients and create a new thread for client.
+ */
+public class Server {
+
+	private ServerSocket serverSocket;
+
+	public Server(ServerSocket socket) {
+		this.serverSocket = socket;
+	}
+
+	/**
+	 * Method to start the server
+	 */
+	public void startServer() {
+		try {
+			while (!serverSocket.isClosed()) {
+				Socket socket = serverSocket.accept();
+				System.out.println("New Client has connected!");
+				ClientHandler clientHandler = new ClientHandler(socket);
+
+				Thread thread = new Thread(clientHandler);
+				thread.start();
+			}
+		} catch (IOException e) {
+
+		}
+	}
+
+	/**
+	 * Method to close the server
+	 */
+	public void closeServer() {
+		try {
+			if (serverSocket != null) {
+				serverSocket.close();
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
