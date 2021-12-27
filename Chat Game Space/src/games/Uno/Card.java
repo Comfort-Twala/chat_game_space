215dd1d47225953194672fde5e333672130db659
Comfort-Twala <kontreitroos@gmail.com>
2021-12-27 22:13:41 +0200

    Modified Card representation

--- Chat Game Space/src/games/Uno/Card.java
+++ Chat Game Space/src/games/Uno/Card.java
@@ -1,5 +1,7 @@
 package games.Uno;
 
+import java.util.Arrays;
+
 /**
  * Card class for Uno cards 
  */
@@ -8,6 +10,7 @@ public class Card {
 	private int number;
 	private Colour colour;
 	private Type type;
+	private String[] card;
 
 	/**
 	 * Colour enum for the different Uno card colours
@@ -42,26 +45,61 @@ public class Card {
 		this.number = number;
 		this.colour = colour;
 		this.type = type;
+		this.card = new String[6];
+	}
+
+	private String centerPad(String str, int totLength) {
+		int pad = (totLength - str.length()) / 2;
+		String result = "";
+		for (int i = 0; i < pad; i++) {
+			result += " ";
+		}
+		result += str;
+		for (int i = 0; i < (totLength - pad - str.length()); i++) {
+			result += " ";
+		}
+		return result;
 	}
 
 	/**
 	 * String representation method for Uno Card
 	 */
 	public String toString() {
-		switch (this.type) {
+		this.card[0] = "-----------";
+		this.card[1] = "|         |";
+		switch (this.colour) {
 			case NONE:
-				return this.number + " | " + this.colour;
-			case DRAW_TWO:
-				return "Take Two" + " | " + this.colour;
-			case SKIP:
-				return "Skip" + " | " + this.colour;
-			case REVERSE:
-				return "Reverse" + " | " + this.colour;
-			case WILD:
-				return "Wild Card";
-			case WILD_DRAW_FOUR:
-				return "Wild Take Four Card";
+				this.card[2] = "|  WILD   |";
+				switch (this.type) {
+					case WILD:
+						this.card[3] = "|  CARD   |";
+						break;
+					case WILD_DRAW_FOUR:
+						this.card[3] = "|   +4    |";
+						break;
+					default:
+						break;
+				}
+				break;
+			case RED:
+				this.card[2] = "|   RED   |";
+				break;
+			case BLUE:
+				this.card[2] = "|  BLUE   |";
+				break;
+			case GREEN:
+				this.card[2] = "|  GREEN  |";
+				break;
+			case YELLOW:
+				this.card[2] = "|  YELLOW |";
+				break;
+			
 		}
-		return null;
+		if (this.colour != Colour.NONE)
+			this.card[3] = "|" + centerPad(Integer.toString(this.number), 9) + "|";
+		this.card[4] = "|         |";
+		this.card[5] = "-----------";
+
+		return Arrays.asList(card).toString();
 	}	
 }
